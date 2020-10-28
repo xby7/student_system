d1ae65478914bc76be3f0f6c894d99ea07614036
cbb <936934118@qq.com>
2020-10-28 17:23:42 +0800

    2.12

--- src/main/java/com/jxd/student_information/controller/UserloginController.java
+++ src/main/java/com/jxd/student_information/controller/UserloginController.java
@@ -4,6 +4,7 @@ import com.jxd.student_information.model.Userlogin;
 import com.jxd.student_information.service.IUserloginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -22,9 +23,12 @@ public class UserloginController {
     private IUserloginService userloginService;
 
 
-    @RequestMapping("/updateUserPWD")
+    @RequestMapping("/updateUserPWD/{user_id}/{newPWD}")
     @ResponseBody
-    public String updateUserPWD(Userlogin userlogin){
+    public String updateUserPWD(@PathVariable("user_id") int user_id,@PathVariable("newPwd") String newPWd){
+        Userlogin userlogin = new Userlogin();
+        userlogin.setUser_id(user_id);
+        userlogin.setPassword(newPWd);
         boolean flag = userloginService.updateById(userlogin);
         if (flag){
             return "success";
