0ac70aac7d16a2f3877b61b92d65170756c24d00
lk <lk@qq.com>
2020-10-30 14:38:45 +0800

    lk 14:40 10.30

--- src/main/java/com/jxd/student_information/mapper/IStudentMapper.java
+++ src/main/java/com/jxd/student_information/mapper/IStudentMapper.java
@@ -2,6 +2,10 @@ package com.jxd.student_information.mapper;
 
 import com.jxd.student_information.model.Student;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -12,5 +16,21 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @since 2020-10-28
  */
 public interface IStudentMapper extends BaseMapper<Student> {
+    /**
+     * 通过姓名、班期查找学生
+     * @return
+     */
+    List<Map<String,Object>> getAllStudent(@Param("student_name")String student_name, @Param("class_no")int class_no);
+
+    /**
+     * 分页查询
+     * @param student_name
+     * @param class_no
+     * @param pageIndex
+     * @param pageSize
+     * @return
+     */
+    List<Map<String,Object>> getAllStudentByPage(@Param("student_name")String student_name, @Param("class_no")int class_no,@Param("pageIndex") int pageIndex, @Param("pageSize") int pageSize);
+
 
 }
