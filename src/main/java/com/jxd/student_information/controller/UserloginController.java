3c4f663b62000d8f0911d680aee752e076b80560
xby <1947304419@qq.com>
2020-11-04 16:15:21 +0800

    11.4 16：06 xby

--- src/main/java/com/jxd/student_information/controller/UserloginController.java
+++ src/main/java/com/jxd/student_information/controller/UserloginController.java
@@ -1,11 +1,12 @@
 package com.jxd.student_information.controller;
 
-import com.jxd.student_information.jwt.JwtUtil;
 import com.jxd.student_information.model.Userlogin;
 import com.jxd.student_information.service.IUserloginService;
+import com.jxd.student_information.utils.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
