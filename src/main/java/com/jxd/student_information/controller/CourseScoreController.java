dfb8916be6614615c66ecbed89694eacbe173a75
xby <1947304419@qq.com>
2020-11-05 11:05:22 +0800

    11.5 11：06 xby

--- src/main/java/com/jxd/student_information/controller/CourseScoreController.java
+++ src/main/java/com/jxd/student_information/controller/CourseScoreController.java
@@ -2,26 +2,16 @@ package com.jxd.student_information.controller;
 
 import com.jxd.student_information.model.CourseScore;
 import com.jxd.student_information.service.ICourseScoreService;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * <p>
- * 前端控制器
- * </p>
- *
- * @author xby
- * @since 2020-10-28
- */
 @Controller
 public class CourseScoreController {
 
