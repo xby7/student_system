c9c5a9ec572a9c2bace7e2021168d6555129619e
xby <1947304419@qq.com>
2020-11-05 14:36:30 +0800

    11.5 14：36 xby

--- src/main/java/com/jxd/student_information/model/Userlogin.java
+++ src/main/java/com/jxd/student_information/model/Userlogin.java
@@ -3,17 +3,9 @@ package com.jxd.student_information.model;
 import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
-/**
- * <p>
- *
- * </p>
- *
- * @author xby
- * @since 2020-10-30
- */
 @Data
 public class Userlogin {
-    //告诉mp表的主键是什么
+
     @TableId
     private Integer userId;
     private String password;
