74ac1c06f13c6b3a8b1f2b9fc43e5235cd6a2e0b
lk <lk@qq.com>
2020-11-06 17:02:35 +0800

    lk

---
+++ src/main/java/com/jxd/student_information/service/impl/StudentServiceImpl.java
@@ -0,0 +1,53 @@
+package com.jxd.student_information.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.jxd.student_information.mapper.IStudentMapper;
+import com.jxd.student_information.model.Student;
+import com.jxd.student_information.service.IStudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author xby
+ * @since 2020-10-28
+ */
+@Service
+public class StudentServiceImpl extends ServiceImpl<IStudentMapper, Student> implements IStudentService {
+    @Autowired
+    private IStudentMapper studentMapper;
+
+    @Override
+    public List<Map<String, Object>> getAllStudent(String student_name, int class_no) {
+        return studentMapper.getAllStudent(student_name,class_no);
+    }
+
+    @Override
+    public List<Map<String, Object>> getAllStudentByPage(String student_name, int class_no, int curPage, int pageSize) {
+        int pageIndex = (curPage-1)*pageSize;
+
+        return studentMapper.getAllStudentByPage(student_name,class_no,pageIndex,pageSize);
+    }
+
+    @Override
+    public boolean addStudents(Student student) {
+        return studentMapper.addStudents(student);
+    }
+
+    @Override
+    public boolean delStudent(Integer studentId) {
+        return studentMapper.delStudent(studentId);
+    }
+
+    @Override
+    public boolean editStudent(Student student) {
+        return studentMapper.editStudent(student);
+    }
+
+}
---
+++ src/main/java/com/jxd/student_information/utils/ImgUtil.java
@@ -0,0 +1,16 @@
+package com.jxd.student_information.utils;
+
+import lombok.Data;
+
+/**
+ * @ClassName ImgUtil
+ * @Description TODO
+ * @Author liukai
+ * @Date 2020/11/3
+ * @Version 1.0
+ */
+@Data
+public class ImgUtil {
+
+    private String imgpath;
+}
