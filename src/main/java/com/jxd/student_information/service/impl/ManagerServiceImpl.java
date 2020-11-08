4a5d5bdbbb2c2be4ed27405898313cf8fce2e593
cbb <936934118@qq.com>
2020-11-08 16:04:52 +0800

    cbb 11.8 16:05

--- src/main/java/com/jxd/student_information/service/impl/ManagerServiceImpl.java
+++ src/main/java/com/jxd/student_information/service/impl/ManagerServiceImpl.java
@@ -53,19 +53,19 @@ public class ManagerServiceImpl extends ServiceImpl<IManagerMapper, Manager> imp
         List<String> dynamicSequence = managerMapper.getSequenceWithManager();
         //手动与固定列拼接
         String sql = "SELECT e.student_id,student_name,manager_id,score,quality_name,e.overall_score dept," +
-                "evaluation_form_dept,eos.overall_score school,q_period,e_period," +
+                "evaluation_form_dept,eos.overall_score school,q_period,e_period,isdeleted," +
                 dynamicSequence.get(dynamicSequence.size() - 1) +
                 " FROM(" +
                 " SELECT d.student_id,student_name,manager_id,score,quality_name,overall_score," +
-                "evaluation_form_dept,d.period_no q_period,eod.period_no e_period" +
+                "evaluation_form_dept,d.period_no q_period,eod.period_no e_period,isdeleted" +
                 " FROM(" +
-                " SELECT c.student_id,student_name,manager_id,score,quality_name,period_no" +
+                " SELECT c.student_id,student_name,manager_id,score,quality_name,period_no,isdeleted" +
                 " FROM(" +
-                " SELECT b.student_id,student_name,manager_id,score,quality_id,period_no" +
+                " SELECT b.student_id,student_name,manager_id,score,quality_id,period_no,isdeleted" +
                 " FROM(" +
-                " SELECT student_id,student_name,manager_id" +
+                " SELECT student_id,student_name,manager_id,isdeleted" +
                 " FROM(" +
-                " SELECT student_id, student_name, d.dept_no FROM student s LEFT JOIN dept d ON d.dept_no = s.dept_no ) a" +
+                " SELECT student_id, student_name, d.dept_no,isdeleted FROM student s LEFT JOIN dept d ON d.dept_no = s.dept_no ) a" +
                 " LEFT JOIN manager m ON a.dept_no = m.dept_no ) b" +
                 " LEFT JOIN quality_score qs ON qs.student_id = b.student_id ) c" +
                 " LEFT JOIN quality q ON q.quality_id = c.quality_id ) d" +
@@ -82,19 +82,19 @@ public class ManagerServiceImpl extends ServiceImpl<IManagerMapper, Manager> imp
         List<String> dynamicSequence = managerMapper.getSequenceWithManager();
         //手动与固定列拼接
         String sql = "SELECT e.student_id,student_name,manager_id,score,quality_name,e.overall_score dept," +
-                "evaluation_form_dept,eos.overall_score school,q_period,e_period," +
+                "evaluation_form_dept,eos.overall_score school,q_period,e_period,isdeleted," +
                 dynamicSequence.get(dynamicSequence.size() - 1) +
                 " FROM(" +
                 " SELECT d.student_id,student_name,manager_id,score,quality_name,overall_score," +
-                "evaluation_form_dept,d.period_no q_period,eod.period_no e_period" +
+                "evaluation_form_dept,d.period_no q_period,eod.period_no e_period,isdeleted" +
                 " FROM(" +
-                " SELECT c.student_id,student_name,manager_id,score,quality_name,period_no" +
+                " SELECT c.student_id,student_name,manager_id,score,quality_name,period_no,isdeleted" +
                 " FROM(" +
-                " SELECT b.student_id,student_name,manager_id,score,quality_id,period_no" +
+                " SELECT b.student_id,student_name,manager_id,score,quality_id,period_no,isdeleted" +
                 " FROM(" +
-                " SELECT student_id,student_name,manager_id" +
+                " SELECT student_id,student_name,manager_id,isdeleted" +
                 " FROM(" +
-                " SELECT student_id, student_name, d.dept_no FROM student s LEFT JOIN dept d ON d.dept_no = s.dept_no ) a" +
+                " SELECT student_id, student_name, d.dept_no,isdeleted FROM student s LEFT JOIN dept d ON d.dept_no = s.dept_no ) a" +
                 " LEFT JOIN manager m ON a.dept_no = m.dept_no ) b" +
                 " LEFT JOIN quality_score qs ON qs.student_id = b.student_id ) c" +
                 " LEFT JOIN quality q ON q.quality_id = c.quality_id ) d" +
